9a675aa4ef7bac54b90905ecbe1ec666a1e69700
krunal9307 <krunalkurkure2721@gmail.com>
2023-02-03 14:10:04 +0530

    first commit

--- src/components/common/app-head.tsx
+++ src/components/common/app-head.tsx
@@ -7,7 +7,7 @@ export function AppHead(): JSX.Element {
       <meta name='og:title' content='Twitter' />
       <link rel='icon' href='/favicon.ico' />
       <link rel='manifest' href='/site.webmanifest' key='site-manifest' />
-      <meta name='twitter:site' content='@ccrsxx' />
+      <meta name='twitter:site' content='@Krunal' />
       <meta name='twitter:card' content='summary_large_image' />
     </Head>
   );
